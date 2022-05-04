6be72de6786d6929d1a653521be953d64e8257b9
Pat Monette <patrick@ontarioseo.ca>
2022-05-04 02:59:03 +0000

    redirect logged out users from shop

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -4,3 +4,17 @@
  * Remove related products output
  */
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+
+// Redirect non-logged-in users
+function vanderlinden_non_client_redirect() {
+    if (
+        ! is_user_logged_in()
+        && (is_woocommerce() || is_cart() || is_checkout())
+    ) {
+        // feel free to customize the following line to suit your needs
+        wp_redirect(home_url());
+        exit;
+    }
+}
+add_action('template_redirect', 'vanderlinden_non_client_redirect');
